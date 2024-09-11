9ba2f84072dbee12316a2592913b088a74baabc1
Sairah Gado <sairahgado@gmail.com>
2024-09-11 17:10:00 +0800

    Update website with new changes

--- myscript.js
+++ myscript.js
@@ -20,6 +20,7 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 const projects = [
     {excel: "Excel", eLink: "https://sg.docworkspace.com/d/sIJzC2orDAbTf77YG"},
+    {excel2: "Excel", eeLink: "https://sg.docworkspace.com/d/sIAfC2orDAcmghbcG"},
     {calcu: "Calculator", cLink: "https://sairah-gado.github.io/Simple-calculator/"},
     {randomQ: "Random Quotes", rLink: "https://sairah-gado.github.io/Random-Quotes/"},
     {rps: "Rock Paper Scissor", rpsLink: "https://sairah-gado.github.io/Rock-Paper-Scissors/"},
@@ -33,39 +34,45 @@ button.innerHTML = `View ${projects[0].excel} Project`;
 button.addEventListener('click', () => {
     window.open(projects[0].eLink, '_blank')
 })
+// excel2
+const button7 = document.getElementById('excel-proj2');
+button7.innerHTML = `View ${projects[1].excel2} Project`;
+button7.addEventListener('click', () => {
+    window.open(projects[1].eeLink, '_blank')
+})
 // calcu
 const button1 = document.getElementById('calcu');
-button1.innerHTML = `View ${projects[1].calcu} Project`;
+button1.innerHTML = `View ${projects[2].calcu} Project`;
 button1.addEventListener('click', () => {
-    window.open(projects[1].cLink, '_blank')
+    window.open(projects[2].cLink, '_blank')
 })
 // random quotes
 const button2 = document.getElementById('random');
-button2.innerHTML = `View ${projects[2].randomQ} Project`;
+button2.innerHTML = `View ${projects[3].randomQ} Project`;
 button2.addEventListener('click', () => {
-    window.open(projects[2].rLink, '_blank')
+    window.open(projects[3].rLink, '_blank')
 })
 // RPS
 const button3 = document.getElementById('rps');
-button3.innerHTML = `View ${projects[3].rps} Project`;
+button3.innerHTML = `View ${projects[4].rps} Project`;
 button3.addEventListener('click', () => {
-    window.open(projects[3].rpsLink, '_blank')
+    window.open(projects[4].rpsLink, '_blank')
 })
 // todo
 const button4 = document.getElementById('todo');
-button4.innerHTML = `View ${projects[4].todo} Project`;
+button4.innerHTML = `View ${projects[5].todo} Project`;
 button4.addEventListener('click', () => {
-    window.open(projects[4].tLink, '_blank')
+    window.open(projects[5].tLink, '_blank')
 })
 // scorer
 const button5 = document.getElementById('score');
-button5.innerHTML = `View ${projects[5].score} Project`;
+button5.innerHTML = `View ${projects[6].score} Project`;
 button5.addEventListener('click', () => {
-    window.open(projects[5].sLink, '_blank')
+    window.open(projects[6].sLink, '_blank')
 })
 // Pokemon API
 const button6 = document.getElementById('pokemon');
-button6.innerHTML = `View ${projects[6].pokemon} Project`;
+button6.innerHTML = `View ${projects[7].pokemon} Project`;
 button6.addEventListener('click', () => {
-    window.open(projects[6].pLink, '_blank')
+    window.open(projects[7].pLink, '_blank')
 })
